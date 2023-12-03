637302b5bfc0eb4c1bebb4636a438b468124c7cc
Sola <dev@sola.love>
2023-12-04 03:51:01 +0800

    day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -3,7 +3,10 @@ use aoc_2023_rust::aoc;
 fn main() {
     let aoc = aoc().unwrap();
     let input = aoc.read_input(3).unwrap();
-    part1(&input);
+
+    let mut nums = Num::parse(&input);
+    part1(&input, &mut nums);
+    part2(&input, &nums);
 }
 
 #[derive(Debug)]
@@ -15,42 +18,62 @@ struct Num {
     found: bool,
 }
 
-fn part1(input: &str) {
-    let mut nums: Vec<Num> = vec![];
-    input.lines().enumerate().for_each(|(y, line)| parse_numbers(y, line, &mut nums));
+impl Num {
+    fn parse(input: &str) -> Vec<Num> {
+        let mut nums: Vec<Num> = vec![];
+        input.lines().enumerate().for_each(|(y, line)| Self::parse_line(y, line, &mut nums));
+        nums
+    }
+
+    fn parse_line(y: usize, line: &str, nums: &mut Vec<Num>) {
+        let mut chars = line.char_indices();
+        while let Some((i, _)) = chars.find(|(_, c)| c.is_numeric()) {
+            let e = match chars.find(|(_, c)| !c.is_numeric()) {
+                Some((j, _)) => j,
+                None => line.len(),
+            };
+            let num = line[i..e].parse().unwrap();
+            nums.push(Num {
+                num,
+                x: i,
+                y,
+                len: e - i,
+                found: false,
+            });
+        }
+    }
+
+    fn touches(&self, x: usize, y: usize) -> bool {
+        let by = y == self.y || y + 1 == self.y || self.y + 1 == y; // same line, above, below
+        let bx = x + 1 == self.x || x == self.x + self.len - 1 || (x >= self.x && x <= self.x + self.len); // before, after, inside
+        by && bx
+    }
+}
+
+fn part1(input: &str, nums: &mut [Num]) {
     input.lines().enumerate().for_each(|(y, line)| {
         line.match_indices(|c: char| !c.is_numeric() && c != '.').for_each(|(x, _)| {
-            mark_found(x, y, &mut nums);
+            nums.iter_mut()
+                .filter(|num| !num.found)
+                .filter(|num| num.touches(x, y))
+                .for_each(|num| num.found = true);
         });
     });
     let sum: u32 = nums.iter().filter(|num| num.found).map(|num| num.num).sum();
     println!("part1: {sum}")
 }
 
-fn parse_numbers(y: usize, line: &str, nums: &mut Vec<Num>) {
-    let mut chars = line.char_indices();
-    while let Some((i, _)) = chars.find(|(_, c)| c.is_numeric()) {
-        let e = match chars.find(|(_, c)| !c.is_numeric()) {
-            Some((j, _)) => j,
-            None => line.len(),
-        };
-        let num = line[i..e].parse().unwrap();
-        nums.push(Num {
-            num,
-            x: i,
-            y,
-            len: e - i,
-            found: false,
+fn part2(input: &str, nums: &[Num]) {
+    let mut sum = 0u32;
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.match_indices('*').for_each(|(x, _)| {
+            let mut parts = nums.iter().filter(|num| num.touches(x, y));
+            let a = parts.next();
+            let b = parts.next();
+            if let (Some(a), Some(b)) = (a, b) {
+                sum += a.num * b.num;
+            }
         });
-    }
-}
-
-fn mark_found(x: usize, y: usize, nums: &mut [Num]) {
-    nums.iter_mut().filter(|num| !num.found).for_each(|num| {
-        let by = y == num.y || y + 1 == num.y || num.y + 1 == y; // same line, above, below
-        let bx = x + 1 == num.x || x == num.x + num.len - 1 || (x >= num.x && x <= num.x + num.len); // before, after, inside
-        if by && bx {
-            num.found = true;
-        }
     });
+    println!("part2: {sum}")
 }
