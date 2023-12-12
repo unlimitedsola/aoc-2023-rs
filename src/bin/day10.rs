b9d3b958a474505d3465f7317a12a941ad581745
Sola <dev@sola.love>
2023-12-12 22:12:21 +0800

    day10 part2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use num::abs;
 
 use aoc_2023_rust::aoc;
 use Direction::{Left, Right};
@@ -10,6 +11,7 @@ fn main() {
     let input = aoc.read_input(10).unwrap();
     let map = Map::parse(&input);
     println!("part1: {}", part1(&map));
+    println!("part2: {}", part2(&map));
 }
 
 fn part1(map: &Map) -> usize {
@@ -31,6 +33,31 @@ fn part1(map: &Map) -> usize {
         mv_cnt += 1;
     }
     mv_cnt - 1
+}
+
+fn part2(map: &Map) -> usize {
+    let (size_x, size_y) = map.size();
+    let mut visited = vec![vec![false; size_x]; size_y];
+    let start = map.starting_pos();
+    let mut stack = vec![start];
+    visited[start.x][start.y] = true;
+    let mut boundary = vec![start];
+    while let Some(pos) = stack.pop() {
+        for pos in map.connects_to(pos) {
+            if !visited[pos.x][pos.y] {
+                visited[pos.x][pos.y] = true;
+                stack.push(pos);
+                boundary.push(pos);
+            }
+        }
+    }
+    let b = boundary.len();
+    // Shoelace: A = sum((y1 + y2) * (x1 - x2)) / 2
+    let area = boundary.into_iter().circular_tuple_windows()
+        .map(|(p1, p2)| (p1.y as i64 + p2.y as i64) * (p1.x as i64 - p2.x as i64))
+        .sum::<i64>() / 2;
+    // Pick's theorem: A = i + b / 2 - 1 => i = A - b / 2 + 1
+    abs(area) as usize - b / 2 + 1
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -58,27 +85,23 @@ struct Pos {
     y: usize,
 }
 
-struct Map {
-    m: Vec<Vec<char>>, // m[y][x]
-}
+struct Map(Vec<Vec<char>>); // [y][x]
 
 impl Map {
     fn parse(input: &str) -> Self {
-        Self {
-            m: input.lines().map(|line| line.chars().collect_vec()).collect_vec()
-        }
+        Self(input.lines().map(|line| line.chars().collect_vec()).collect_vec())
     }
 
     fn size(&self) -> (usize, usize) {
-        (self.m[0].len(), self.m.len())
+        (self.0[0].len(), self.0.len())
     }
 
     fn get(&self, pos: Pos) -> char {
-        self.m[pos.y][pos.x]
+        self.0[pos.y][pos.x]
     }
 
     fn starting_pos(&self) -> Pos {
-        self.m.iter().enumerate().find_map(|(y, line)| {
+        self.0.iter().enumerate().find_map(|(y, line)| {
             line.iter().enumerate().find_map(|(x, &c)| {
                 if c == 'S' {
                     Some(Pos { x, y })
@@ -90,7 +113,9 @@ impl Map {
     }
 
     fn connects_to(&self, pos: Pos) -> Vec<Pos> {
-        self.get(pos).connects().into_iter().filter_map(|d| self.nav(pos, d)).collect_vec()
+        [Up, Down, Left, Right].into_iter()
+            .filter(|&d| self.get(pos).connects(d))
+            .filter_map(|d| self.nav(pos, d)).collect_vec()
     }
 
     fn nav(&self, pos: Pos, direction: Direction) -> Option<Pos> {
@@ -103,34 +128,34 @@ impl Map {
             Right if x + 1 < size_x => Some(Pos { x: x + 1, y }),
             _ => None,
         };
-        pos.filter(|&pos| self.get(pos).accepts(direction.opposite()))
+        pos.filter(|&pos| self.get(pos).accepts(direction))
     }
 }
 
 trait Pipe {
-    fn connects(&self) -> Vec<Direction>;
+    fn connects(&self, direction: Direction) -> bool;
     fn accepts(&self, direction: Direction) -> bool;
 }
 
 impl Pipe for char {
-    fn connects(&self) -> Vec<Direction> {
-        match self {
-            '|' => vec![Up, Down],
-            '-' => vec![Left, Right],
-            'L' => vec![Up, Right],
-            'J' => vec![Up, Left],
-            '7' => vec![Left, Down],
-            'F' => vec![Down, Right],
-            'S' => vec![Up, Down, Left, Right],
-            _ => vec![],
-        }
+    fn connects(&self, to: Direction) -> bool {
+        matches!(
+            (self, to),
+            ('|', Up | Down) |
+            ('-', Left | Right) |
+            ('L', Up | Right) |
+            ('J', Up | Left) |
+            ('7', Left | Down) |
+            ('F', Down | Right) |
+            ('S', _)
+        )
     }
 
-    fn accepts(&self, direction: Direction) -> bool {
+    fn accepts(&self, from: Direction) -> bool {
         if *self == 'S' {
             false
         } else {
-            self.connects().contains(&direction)
+            self.connects(from.opposite())
         }
     }
 }
